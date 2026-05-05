944c6270fbf0d3653a98523c2d427ba27c632829
sayyad-shahin <shahin.s@crbix.in>
2026-05-05 13:24:47 +0530

    fix index.js remove strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement(React.StrictMode, null, React.createElement(App, null)));
+root.render(React.createElement(App, null));
